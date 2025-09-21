2489236fab2a6047c792ee0ab4fcbb2f55627d1a
obata-hiroshi <obata.hiroshi@gmail.com>
2025-09-21 15:58:45 +0900

    ios

---
+++ ios/time-x/time-x/time_xApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_xApp.swift
+//  time-x
+//
+//  Created by 小畑浩 on 2025/09/21.
+//
+
+import SwiftUI
+
+@main
+struct time_xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/time-x/time-xTests/time_xTests.swift
@@ -0,0 +1,17 @@
+//
+//  time_xTests.swift
+//  time-xTests
+//
+//  Created by 小畑浩 on 2025/09/21.
+//
+
+import Testing
+@testable import time_x
+
+struct time_xTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
